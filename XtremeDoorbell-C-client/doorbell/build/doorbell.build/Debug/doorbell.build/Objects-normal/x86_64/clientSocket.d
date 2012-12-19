fc14465d029712507b6c3b19f4de4104e74605c4
Allan Baril <allan.baril@gmail.com>
2012-12-18 21:10:39 -0500

    AB - Removed build directory

--- XtremeDoorbell-C-client/doorbell/build/doorbell.build/Debug/doorbell.build/Objects-normal/x86_64/clientSocket.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/allan/Documents/Workspace/XtremeDoorbell/XtremeDoorbell-C-client/doorbell/doorbell/clientSocket.cpp
--- XtremeDoorbell-C-client/doorbell/build/doorbell.build/Debug/doorbell.build/Objects-normal/x86_64/commandHandler.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/allan/Documents/Workspace/XtremeDoorbell/XtremeDoorbell-C-client/doorbell/doorbell/commandHandler.cpp \
-  /Users/allan/Documents/Workspace/XtremeDoorbell/XtremeDoorbell-C-client/doorbell/doorbell/commandHandler.h \
-  /Users/allan/Documents/Workspace/XtremeDoorbell/XtremeDoorbell-C-client/doorbell/doorbell/clientSocket.h \
-  /Users/allan/Documents/Workspace/XtremeDoorbell/XtremeDoorbell-C-client/doorbell/doorbell/settings.h
